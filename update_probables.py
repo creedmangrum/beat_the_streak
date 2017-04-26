b4c057dc83afedc5d94aff697c00649e8d9c9f45
creedmangrum <creed.mangrum@gmail.com>
2017-04-26 09:49:25 -0400

    update probables sheet

--- update_probables.py
+++ update_probables.py
@@ -8,8 +8,11 @@ month = today.month
 day = today.day
 year = today.year
 
-
-filename = 'steamer_ros/' + str(month) + str(day) + str(year) + '/' + 'steamer_ros_pitching_' + str(month) + str(day) + str(year) + '.csv'
+directory = 'steamer_ros/' + str(month) + str(day) + str(year) + '/'
+filename = directory + 'steamer_ros_pitching_' + str(month) + str(day) + str(year) + '.csv'
+output_filename = directory + 'steamer_row_pitching_probables_' + str(month) + str(day) + str(year) + '.csv'
+output_file = open(output_filename, 'wb')
+output_writer = csv.writer(output_file)
 
 todays_games = mlbgame.day(year, month, day)
 game_probables = [[game.p_pitcher_away, game.p_pitcher_home] for game in todays_games]
@@ -17,8 +20,12 @@ probables = [pitcher for game in game_probables for pitcher in game]
 
 with open(filename) as csvfile:
 	reader = csv.reader(csvfile)
-	for row in reader:
-		if len(row[0].split(' ')) > 1:
-			first_init_last = row[0].split(' ')[0][0] + '. ' + row[0].split(' ')[1]
-			if first_init_last in probables:
-				print(first_init_last + ', K/9: ' + str(row[15]))
+	for i, row in enumerate(reader):
+		if i == 0:
+			output_writer.writerow(row)
+		else:
+			if len(row[0].split(' ')) > 1:
+				first_init_last = row[0].split(' ')[0][0] + '. ' + row[0].split(' ')[1]
+				if first_init_last in probables:
+					output_writer.writerow(row)
+					print(first_init_last + ', K/9: ' + str(row[15]))
