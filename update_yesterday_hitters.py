a7ce2e3851cc7991ae069d0ea2129bb195ab2cf3
creedmangrum <creed.mangrum@gmail.com>
2017-04-27 11:09:36 -0400

    better updated hitters

--- update_yesterday_hitters.py
+++ update_yesterday_hitters.py
@@ -26,9 +26,7 @@ for game in yesterdays_games:
 		print(game.game_id)
 		continue
 
-	batters = stats['home_batting'] + stats['away_batting']
-
-	for player in batters:
+	for player in stats['home_batting']:
 		name = player.name_display_first_last
 		if player.ab + player.bb + player.sf + player.sac + player.hbp > 2:
 			yesterdays_player_stats['_'.join(player.name_display_first_last.split(' '))] = {
@@ -37,7 +35,26 @@ for game in yesterdays_games:
 				'game_bb': player.bb,
 				'game_sf': player.sf,
 				'game_sac': player.sac,
-				'game_hbp': player.hbp
+				'game_hbp': player.hbp,
+				'opposing_pitcher': stats['away_pitching'][0].name_display_first_last,
+				'opposing_team': game.away_team,
+				'stadium': game.home_team
+				#batting order at some point
+			}
+
+	for player in stats['away_batting']:
+		name = player.name_display_first_last
+		if player.ab + player.bb + player.sf + player.sac + player.hbp > 2:
+			yesterdays_player_stats['_'.join(player.name_display_first_last.split(' '))] = {
+				'game_hits': player.h,
+				'game_ab': player.ab,
+				'game_bb': player.bb,
+				'game_sf': player.sf,
+				'game_sac': player.sac,
+				'game_hbp': player.hbp,
+				'opposing_pitcher': stats['home_pitching'][0].name_display_first_last,
+				'opposing_team': game.home_team,
+				'stadium': game.home_team
 				#batting order at some point
 			}
 
@@ -45,7 +62,7 @@ with open(filename) as csvfile:
 	reader = csv.reader(csvfile)
 	for i, row in enumerate(reader):
 		if i == 0:
-			new_row = row + ['game_hits', 'game_ab', 'game_pa']
+			new_row = row + ['game_hits', 'game_ab', 'game_pa', 'opposing_pitcher', 'opposing_team', 'stadium']
 			output_writer.writerow(new_row)
 		else:
 			player_name = row[0]
@@ -57,6 +74,6 @@ with open(filename) as csvfile:
 			player_game_ab = player_game_stats.get('game_ab', 0)
 			player_game_pa = player_game_stats.get('game_ab', 0) + player_game_stats.get('game_bb', 0) + player_game_stats.get('game_sf', 0) + player_game_stats.get('game_sac', 0) + player_game_stats.get('game_hbp', 0)
 			if player_game_pa > 2:
-				new_row = row + [player_game_hits, player_game_ab, player_game_pa]
+				new_row = row + [player_game_hits, player_game_ab, player_game_pa, player_game_stats.get('opposing_pitcher', ' '), player_game_stats.get('opposing_team', ' '), player_game_stats.get('stadium', ' ')]
 				output_writer.writerow(new_row) 
 
